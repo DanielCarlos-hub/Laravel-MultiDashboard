1686b0e7e6c9684cfb37c464b24af08ff405bdd9
Daniel Carlos <danielcarlossoares@gmail.com>
2020-10-12 23:57:20 -0300

    npm-forge fix

--- public/js/app.js
+++ public/js/app.js
@@ -37341,8 +37341,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\danielcarlos.net\danielcarlos\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\danielcarlos.net\danielcarlos\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\cursos\Laravel-MultiDashboard-master\Laravel-MultiDashboard\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\cursos\Laravel-MultiDashboard-master\Laravel-MultiDashboard\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
